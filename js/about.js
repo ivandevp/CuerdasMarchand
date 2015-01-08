9b55e7623dcd27950d22f99950adc01416174168
ErikMSP <narersas_474_95@hotmail.com>
2015-01-08 01:11:32 -0500

    Cambios-About-VictorMarchand

--- js/about.js
+++ js/about.js
@@ -1,19 +1,31 @@
-function openPopUp(){
-	$('#capa').fadeIn('fast', function() {
-        $('#popup').css('display','fixed')
-        $('#popup').animate({'left':'60%'},500)
-    });	
+function openPopUp()
+{
+	alert("hola");
+	$('#capa').fadeIn(1000, function(){
+			$('#popup').css('display','inline-block');
+			$('#popup').animate({
+				left:'25%',
+			},'slow');
+			
+	}) ;
+}
+
+function openas(){
+	alert("abierto");
 }
 
 function closePopUp() 
 {
     $('#popup').animate({'left':'-100%'}, 500, function() {
-        $('#popup').css('left','100%')
+        $('#popup').css({
+			'left':'100%',
+		});
         $('#capa').fadeOut('slow')
     });
 }
 
-function cambiaTamano(valor,ancho,alto){
+function cambiaTamano(valor,ancho,alto)
+{
 		var  img = document.getElementById(valor);
 		img.width = ancho;
 		img.height = alto;
